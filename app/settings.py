b1cdeae44a8aa240ea11d5182d867b7537d3161a
Vivek <vivek.159357@gmail.com>
2021-04-07 15:34:49 +0530

    Added DB connection

--- app/settings.py
+++ app/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': "127.0.0.1",
+        'PORT': 5432,
+        'NAME': 'app',
+        'USER': 'postgres',
+        'PASSWORD': 'vivek'
     }
 }
 
